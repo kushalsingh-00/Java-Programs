e59690027f0d9c84f396f59df3088e424e4f58dd
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-01 01:47:05 +0530

    Methods

---
+++ src/com/ksr/Addition.java
@@ -0,0 +1,11 @@
+package com.ksr;
+
+public class Addition {
+    static int add(int a,int b) {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int x=add(5,6);
+        System.out.println(x);
+    }
+}
---
+++ src/com/ksr/Age.java
@@ -0,0 +1,18 @@
+package com.ksr;
+
+public class Age {
+     static void checking(int a) {
+        if(a<18)
+        {
+            System.out.println("Access Denied!");
+        }
+        else
+        {
+            System.out.println("Access Granted!");
+        }
+    }
+
+    public static void main(String[] args) {
+        checking(20);
+    }
+}
