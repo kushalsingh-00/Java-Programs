90d8b6f8c82c246b4aa62fff36200a266073a114
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-01 22:47:51 +0530

    Creating classes and object obj1 from class C1

---
+++ src/com/classes/C1.java
@@ -0,0 +1,11 @@
+package com.classes;
+
+public class C1 {
+    int x=10;
+
+
+    public static void main(String[] args) {
+        C1 myObj=new C1();
+        System.out.println(myObj.x);
+    }
+}
