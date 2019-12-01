7edd92fd71ec538f1d2c66fc4ffe3c48bd907891
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-01 23:12:30 +0530

    making method in a class and call it in main

---
+++ src/com/classes/C3.java
@@ -0,0 +1,11 @@
+package com.classes;
+
+public class C3 {
+    static void myMethod() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String args[]) {
+        myMethod();
+    }
+}
