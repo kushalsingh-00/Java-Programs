03bbdfefd2ebc482cc7ea1f9e53449148618eb05
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-01 23:40:25 +0530

    difference between static and public method

--- src/com/classes/C4.java
+++ src/com/classes/C4.java
@@ -2,11 +2,19 @@ package com.classes;
 
 public class C4 {
     static void myStaticMethod() {
-        System.out.println("Static method can be called without creating an object");
+        System.out.println("Static method can be called without creating an object.");
     }
 
     public void myPublicMethod() {
-        System.out.println("Public method can be called by creating an object");
+        System.out.println("Public method can be called by creating an object.");
+    }
+
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can.");
+    }
+
+    public void speed(int maxSpeed){
+        System.out.println("the max speed of car is:"+maxSpeed);
     }
 
     public static void main(String args[]) {
