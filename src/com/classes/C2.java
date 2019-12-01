e4cdff715e4cc2a116f7f26de07cd8eb8e94710b
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-01 23:04:21 +0530

    Creating object obj2 of class c1 and access it in class C2 and over ridding and final in attribute

--- src/com/classes/C2.java
+++ src/com/classes/C2.java
@@ -1,8 +1,12 @@
 package com.classes;
 
 public class C2 {
+    final int y=4;   // cannot change the value of y or not able to override it
     public static void main(String[] args) {
         C1 myObj2 = new C1();
+        C2 myObj1=new C2();
+        myObj2.x=20;
+        System.out .println(myObj1.y);
         System.out.println(myObj2.x+5);
     }
 }
