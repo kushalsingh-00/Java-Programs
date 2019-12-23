663c3e649985fd61810c7bc952cbea8c2c84c0cf
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-23 13:05:36 +0530

    concept of interface

---
+++ src/com/ksr/Interface.java
@@ -0,0 +1,40 @@
+package com.ksr;
+
+interface Writer
+{
+    void write();
+}
+class Pen implements Writer
+{
+    public void write()
+    {
+        System.out.println("Im a Pen");
+    }
+}
+
+class Pencil implements Writer
+{
+    public void write()
+    {
+        System.out.println("Im a Pencil");
+    }
+}
+class Kit
+{
+    public void doSomething(Writer p)
+    {
+        p.write();
+    }
+}
+
+public class Interface {
+    public static void main(String[] args)
+    {
+        Kit k = new Kit();
+        Writer p = new Pen();
+        Writer pc = new Pencil();
+
+
+        k.doSomething(pc);
+    }
+}
