72901338409c9cba83cbbe5530b0596ff5bb9b01
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-28 22:58:19 +0530

    use to print current time

---
+++ src/com/classes/C21.java
@@ -0,0 +1,10 @@
+package com.classes;
+
+import java.time.LocalTime; // import the LocalTime class
+
+public class C21 {
+    public static void main(String[] args) {
+        LocalTime myObj = LocalTime.now();
+        System.out.println(myObj);
+    }
+}
