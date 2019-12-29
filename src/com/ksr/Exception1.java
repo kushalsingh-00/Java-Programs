c24c83a36e8224b89a5364fa8770bb5cf4e156ba
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-29 22:28:43 +0530

    we also use multiple catche statements and exception e is come in last otherwise it throw error

--- src/com/ksr/Exception1.java
+++ src/com/ksr/Exception1.java
@@ -3,11 +3,23 @@ package com.ksr;
 public class Exception1 {
     public static void main(String[] args) {
         try {
+
+            int arr[]=new int[5];
+            arr[6]=5;
             int i = 2 / 0;
             System.out.println("Hello");        // after exception no statements will not executes
-        } catch (Exception e) {
-            System.out.println("Error");       // instead of thowing a erroe it print what we want in black text
+        } catch (ArithmeticException e) {
+            System.out.println("Error");       // instead of throwing a error it print what we want in black text
             System.err.println("a");           // err change the color to red
+        }
+        catch(ArrayIndexOutOfBoundsException e)      // this directly come to this catch and not execute other statements
+        {
+            System.out.println("please enter in limit");
+        }
+
+        catch(Exception e)         // if we dont know the exceptions type then all type of exception are sub class of this
+        {
+
         }
         finally
         {
