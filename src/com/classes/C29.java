d2e5306be845152a0e8661d7139294299e9d8d5f
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-21 20:58:43 +0530

    Wrappr class and convert to string

---
+++ src/com/classes/C29.java
@@ -0,0 +1,16 @@
+package com.classes;
+
+public class C29 {
+    public static void main(String[] args) {
+        Integer myInt = 5;
+        Double myDouble = 5.99;
+        Character myChar = 'A';
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+
+        Integer myInt1 = 100;
+        String myString = myInt1.toString();
+        System.out.println(myString.length());
+    }
+}
