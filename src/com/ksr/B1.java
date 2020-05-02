a094d05300b6f9322edbd7082dd78e86369db084
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-05-02 18:00:00 +0530

    Revert "ballon problem"
    This reverts commit 594496b5

--- src/com/ksr/B1.java
+++ src/com/ksr/B1.java
@@ -1,11 +1,11 @@
-package com.HackerEarth;
+package com.ksr;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class B6 {
+public class B1 {
     public static void main(String[] args) throws IOException {
         int cycle,n;
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
